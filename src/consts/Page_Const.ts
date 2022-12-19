0642f12bd0160aeba42cc041fd3f066fccb1c5d6
open sky <opensky74102@gmail.com>
2022-12-19 07:24:03 -0600

    add some features

---
+++ src/consts/Page_Const.ts
@@ -0,0 +1,8 @@
+const ANGLELIST = [30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120];
+const ANTENALIST = ['antena1', 'antena2', 'antena3', 'antena4'];
+
+export {
+  ANGLELIST,
+  ANTENALIST,
+  
+}
