7f360de7f4365d9d9a730d7e8651acefca9022fb
open sky <opensky74102@gmail.com>
2022-12-20 09:28:47 -0600

    fixed footer

--- src/consts/Page_Const.ts
+++ src/consts/Page_Const.ts
@@ -1,5 +1,5 @@
 const ANGLELIST = [30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,110,120];
-const ANTENALIST = ['antena1', 'antena2', 'antena3', 'antena4'];
+const ANTENALIST = ['PTP', 'PTMP'];
 
 export {
   ANGLELIST,
